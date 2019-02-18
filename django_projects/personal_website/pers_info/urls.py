229135867889f3c87462934c51b9814461cb9755
Zhilong Li <zhilong_l@yahoo.com>
2019-02-18 00:30:19 -0500

    travel section started

--- django_projects/personal_website/pers_info/urls.py
+++ django_projects/personal_website/pers_info/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  path('travels/', views.travels, name='travels'),
+  path('projects/', views.projects, name='projects'),
+  path('classes/', views.classes, name='classes'),
+
 ]
--- django_projects/personal_website/pers_info/views.py
+++ django_projects/personal_website/pers_info/views.py
@@ -3,5 +3,22 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hi, welcome to Zhilong's homepage")
-    
+    about = "I am currently a senior in computer science and mechanical engineering at Cornell University.\
+    I love learning about anything related to these two subjects; recently I also started \
+    reading about history. This website, built using Django in Python, is a personal project to \
+    have an understanding of web development."
+    context = {'about_me': about}
+    return render(request, 'index.html', context=context)
+
+
+def travels(request):
+    return render(request, 'travels.html')
+
+def projects(request):
+    return HttpResponse("Here is the list of projects that I have worked on, \
+    either for school coursework or for personal interest. Click on each one \
+    of them for more information.")
+
+def classes(request):
+    return HttpResponse("Here are the classes that I have taken in my three years \
+    at Cornell.")
